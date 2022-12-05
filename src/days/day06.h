1bf6bbbf06b054d8484b20abc9c36a0133bb2eaf
Denis Ilyutin <ilyutind@gmail.com>
2022-12-05 10:10:50 +0200

    day06 boilerplate

---
+++ src/days/day06.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "Common.h"
+
+template<>
+std::string PuzzleApproach<6, 1>::RunTest(std::istream& stream)
+{
+	NOT_IMPLEMENTED_PUZZLE;
+}
+
+
+template<>
+std::string PuzzleApproach<6, 2>::RunTest(std::istream& stream)
+{
+	NOT_IMPLEMENTED_PUZZLE;
+}
